a135bd0521c368d2de2fe8f063d1c345b19b92ee
DevLonnos <luisfelipedasilvaaraujo18@gmail.com>
2024-03-19 22:21:52 -0300

    Incluir prototipos das Telas Aplicativo BooPeep.

---
+++ src/app/index.tsx
@@ -0,0 +1,93 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, KeyboardAvoidingView, ScrollView, Image, TextInput, TouchableOpacity } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function Principal() {
+
+    return(
+        <KeyboardAvoidingView style={styles.background}>
+
+            <View style={styles.containerLogo}>
+                <Image
+                style={{
+                    width: 480,
+                    height: 330,
+                }}
+                source={require('../../assets/3-removebg-preview.png')}
+                />
+            </View>
+
+            <View style={styles.container}>
+                <TouchableOpacity style={styles.btnSubmit}>
+                  <Link href={'/Login'}>
+                    <Text style={styles.submitText}>Entrar</Text>
+                  </Link>
+                </TouchableOpacity>
+
+
+                <TouchableOpacity style={styles.btnSubmit2}>
+                    <Link href={'/Cadastro'}>
+                    <Text style={styles.submitText2}>Registrar-se</Text>
+                    </Link>
+                </TouchableOpacity>
+            </View>
+        </KeyboardAvoidingView>
+    )
+}
+
+const styles = StyleSheet.create ({
+    background: {
+        flex: 1,
+        backgroundColor: '#400096',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+    container: {
+        flex: 1,
+        backgroundColor: '#400096',
+        width: '90%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    containerLogo: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: "center",
+        marginBottom: 0,
+      },
+    btnSubmit: {
+         backgroundColor: "#7b83ff",
+         width: "100%",
+         height: 55,
+         alignItems: "center",
+         justifyContent: "center",
+         borderRadius: 10,
+         borderWidth: 2,
+         borderColor: "#000000",
+         marginTop: 120,
+    },
+    submitText: {
+        color: "#fff",
+        fontSize: 18,
+        fontWeight: "bold",
+      },
+    btnSubmit2: {
+        backgroundColor: "#fff",
+        width: "100%",
+        height: 55,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 10,
+        borderWidth: 2,
+        borderColor: "#000000",
+        marginTop: 20,
+   },
+      
+    submitText2: {
+        color: "#400096",
+        fontSize: 18,
+        fontWeight: "bold",
+      },
+
+
+})
